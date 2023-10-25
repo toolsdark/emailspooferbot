38c2942b22836a150d69440a5c89d8213903a7e0
Darkey <148985917+toolsdark@users.noreply.github.com>
2023-10-25 14:55:31 -0300

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -1,11 +1,39 @@
 
-# Create a custom keyboard with buttons
-keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-testing_button = types.KeyboardButton('/testing')
-help_button = types.KeyboardButton('/help')
-pricing_button = types.KeyboardButton('/pricing')
-get_license_button = types.KeyboardButton('/getlicense')  # Add the "Get License" button
-keyboard.row(testing_button, get_license_button)  # Include the "Get License" button in the same row
-keyboard.row(help_button, pricing_button)  # Include the other buttons in the second row
+import telebot
+import smtplib
+from emailsender_bot import EmailSender, EmailMessage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from telebot import types
 
-# If you want whole source code please contact me for purchasing --> @toolsdark
+
+# This is not a complete code for sending email with telegram bot its a complex process but contact @toolsdark for more details !!
+
+
+# Please note it is just a format as i couldnot reveal source code, lookup our bot for test mails:- @tooldark_bot
+
+
+bot = telebot.TeleBot('TOKEN')
+
+
+# Initialize the EmailSender bot
+email_bot = EmailSender(provider='smtp', apiKey='API_KEY')
+
+# Create an EmailMessage object
+email_message = EmailMessage()
+email_message.set_subject('Hello, World!')
+email_message.set_recipient('recipient@example.com')
+email_message.set_text('This is a test email sent from EmailSender Bot.')
+
+# Attach a file if needed
+email_message.attach_file('attachment.pdf')
+
+# Send the email
+response = email_bot.send_email(email_message)
+
+if response.status_code == 200:
+    print("Email sent successfully!")
+else:
+    print("Email sending failed.")
+
+bot.polling()
